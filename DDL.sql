1b997b492ea9b2809d9131d767febccee66f593b
youzhi.cheng <youzhi.cheng@mail.hypers.com>
2022-03-04 18:29:16 +0800

    update table

--- DDL.sql
+++ DDL.sql
@@ -1,14 +1,12 @@
 drop table products;
-create table products(id int primary key ,name varchar(20) not null ,field varchar(20),type varchar (20));
-insert into products(id,name,field,type) values (1,'粉末S33','1','粉末'),(2,'溶液T3','1','溶液'),
-(3,'溶液T6','2','溶液'),(4,'溶液T8','3','溶液'),(5,'产品5','4','粉末');
+create table products(id int primary key ,name varchar(20) not null ,field varchar(20),picture varchar(20),description varchar(20));
+insert into products(id,name,field,picture,description) values (1,'粉末S33','1','logo.jpg','这是粉末S33的简介'),(2,'溶液T3','1','logo.jpg','这是溶液T3'),
+(3,'溶液T6','2','logo.jpg','这是溶液T6%'),(4,'溶液T8','3','logo.jpg','这是溶液T8'),(5,'离型膜','4','logo.jpg','这是溶液T8');
 
 drop  table product_detail;
 CREATE TABLE product_detail (id int primary key ,name varchar(20) not null ,picture varchar(20),description varchar(20),field varchar(20));
 insert into product_detail(id, name, picture, description,field) values (1,'粉末S33','logo.jpg','这是粉末S33的简介','1'),(2,'溶液T3','logo.jpg','这是溶液T3','1'),(3,'溶液T6','logo.jpg','这是溶液T6%','3'),
 (4,'溶液T8','logo.jpg','这是溶液T8','4');
-
-insert into products(id, name, field, type) value (6,'产品6','4','溶液')
 
 
 
